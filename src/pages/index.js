4fd2fe04d8f328e61cb54df07d02477f5a47dadc
Colin Snyder <86389945+colinstartneon@users.noreply.github.com>
2021-10-02 12:43:39 -0700

    The End of the Beginning

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,14 +8,12 @@ import { Helmet } from "react-helmet";
 function IndexPage() {
   return (
     <Layout>
-      <Helmet></Helmet>
+      <Helmet>
+        <script src="https://engine.banabo.io/embeddable/index.js"></script>
+      </Helmet>
       <GlobalFonts />
       <SEO title="Home" />
       <FullIndexPage />
     </Layout>
   );
 }
-
-export default IndexPage;
-
-// <script src="https://engine.banabo.io/embeddable/index.js"></script>
