d825604cf82317fe5216bd2872234ceeb0c8abe8
Colin Snyder <86389945+colinstartneon@users.noreply.github.com>
2021-10-03 20:44:00 -0700

    Fixed

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -3,9 +3,6 @@ import Layout from "../components/layout/layout";
 import SEO from "../components/layout/seo";
 import FullContactPage from "../components/pages/FullContactPage";
 import { Helmet } from "react-helmet";
-import * as FullStory from "@fullstory/browser";
-
-FullStory.init({ orgId: "12ZTK0" });
 
 function ContactPage() {
   return (
