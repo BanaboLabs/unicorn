311fd0e43b4d9c97f3c82bf50a462f4f6b8a2a02
Colin Snyder <86389945+colinstartneon@users.noreply.github.com>
2021-10-03 19:51:43 -0700

    Added headers

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -2,10 +2,14 @@ import React from "react";
 import Layout from "../components/layout/layout";
 import SEO from "../components/layout/seo";
 import FullContactPage from "../components/pages/FullContactPage";
+import { Helmet } from "react-helmet";
 
 function ContactPage() {
   return (
     <Layout>
+      <Helmet>
+        <script src="https://engine.banabo.io/embeddable/index.js"></script>
+      </Helmet>
       <SEO title="Contact" />
       <FullContactPage />
     </Layout>
