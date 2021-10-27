0de43976148ab1fb60b22492ba636823b0795911
Colin Snyder <86389945+colinstartneon@users.noreply.github.com>
2021-10-27 13:09:40 -0700

    Changed signup to landing

--- src/components/individual/HeaderBlack.js
+++ src/components/individual/HeaderBlack.js
@@ -41,7 +41,7 @@ export default function HeaderBlack() {
                 <TabItem>Login</TabItem>
               </StyledLink>
             </MobileHidden>
-            <StyledLink to="https://app.banabo.io/signup">
+            <StyledLink to="/landing">
               <Button>Sign Up</Button>
             </StyledLink>
           </SubGroup2>
