5bfff6157847c0c8e4614e83e3b16c232d2ed742
Colin Snyder <86389945+colinstartneon@users.noreply.github.com>
2021-10-30 18:57:11 -0700

    test

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,14 @@
-import React from "react";
+import React, { useEffect } from "react";
 import FullIndexPage from "../components/pages/FullIndexPage";
 import Layout from "../components/layout/layout";
 import SEO from "../components/layout/seo";
 import GlobalFonts from "../fonts/fonts";
 import { Helmet } from "react-helmet";
+
+useEffect(() => {
+  var referrer = document.referrer;
+  console.log("referrer url", referrer);
+}, []);
 
 function IndexPage() {
   return (
