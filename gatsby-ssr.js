82c4377373bf6e99d6c2d62285df3c9a288f74c7
Colin Snyder <86389945+colinstartneon@users.noreply.github.com>
2022-01-05 10:49:28 -0800

    css fix

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -5,3 +5,16 @@
  */
 
 // You can delete this file if you're not using it
+
+import { CSSReset } from "";
+import { GlobalCSS } from "";
+
+export const wrapPageElement = ({ element }) => {
+  return (
+    <>
+      <CSSReset />
+      <GlobalCSS />
+      {element}
+    </>
+  );
+};
