b4323c0b9a66d291813d3fa83097c02ac150face
Colin Snyder <86389945+colinstartneon@users.noreply.github.com>
2022-01-05 12:55:08 -0800

    test

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,13 @@
 //
 module.exports = {
+  siteMetadata: {
+    title: `Banabo`,
+    description: `With Banabo, you can completely map every unique visitor's journey with your brand through conversion.`,
+    author: `@Colin & Ben`,
+    keywords: `marketing attribution, channel growth, cross-channel attribution scores, multi-channel conversions, startup marketing software`,
+    siteUrl: "https://banabo.io",
+    // Need to add image metadata
+  },
   plugins: [
     {
       resolve: `gatsby-plugin-styled-components`,
