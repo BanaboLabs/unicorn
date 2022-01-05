e6589b56c3b8018eb6fc25372fa8091229113183
Colin Snyder <86389945+colinstartneon@users.noreply.github.com>
2022-01-05 14:13:09 -0800

    test

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,10 +1,8 @@
-/**
- * Implement Gatsby's SSR (Server Side Rendering) APIs in this file.
- *
- * See: https://www.gatsbyjs.com/docs/ssr-apis/
- */
+const React = require("react");
+const Layout = require("./src/components/layout/layout").default;
 
-// You can delete this file if you're not using it
-import CustomLayout from "./wrapPageElement";
-
-export const wrapPageElement = CustomLayout;
+exports.wrapPageElement = ({ element, props }) => {
+  // props provide same data to Layout as Page element will get
+  // including location, data, etc - you don't need to pass it
+  return <Layout {...props}>{element}</Layout>;
+};
